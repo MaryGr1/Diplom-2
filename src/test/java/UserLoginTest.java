568e8f1ca9310e38167b3e35f81d8189b1a54325
Mary Grinkova <mashkecha2017@ya.ru>
2025-08-11 12:12:41 +0300

    .idea delete

--- src/test/java/UserLoginTest.java
+++ src/test/java/UserLoginTest.java
@@ -5,6 +5,7 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.example.User;
 import org.example.UserSteps;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -70,6 +71,15 @@ public class UserLoginTest {
                 .loginUser(fakeUser)
                 .statusCode(401)
                 .body("success", is(false));
+    }
+
+    @After
+    public void deleteUser() {
+        try {
+            userSteps.userDeleteAfterLogin(user);
+        } catch (Exception e) {
+            System.err.println("Ошибка при удалении пользователя: " + e.getMessage());
+        }
     }
 
 
